3585e6dc47eff1dc5a593c9337d0e5c8c18b8c43
Mryesiller <gokselyesiller@gmail.com>
2022-07-01 12:27:31 +0300

    css files updated

--- src/components/Screen/Screen.jsx
+++ src/components/Screen/Screen.jsx
@@ -1,23 +1,16 @@
-import React from "react"
+import React, { Fragment } from "react"
 import classes from "./Screen.module.css"
-import { CSSTransition } from "react-transition-group"
 
 function Screen(props) {
   return (
-    <div className={classes.screen}>
-      <CSSTransition
-        in={props.skip}
-        timeout={300}
-        className={classes.transition}
-      >
-        <img src={props.songs.img} alt={props.songs.artist}></img>
-      </CSSTransition>
+    <Fragment>
+      <img src={props.songs.img} alt={props.songs.artist}></img>
       <div className={classes.text}>
         <h3>
           {props.songs.artist}-{props.songs.song}
         </h3>
       </div>
-    </div>
+    </Fragment>
   )
 }
 
